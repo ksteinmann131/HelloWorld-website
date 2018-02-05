affbe00a1963842da9885ef2f2134f8069de3585
Kelsey Steinmann <kelseysteinmann@StaceyspleWatch.localdomain>
2018-02-04 17:09:44 -0700

    added port variable for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => console.log("this is working, port 3000"));
+app.listen(port, () => console.log("Served on " + port));
